b3a5770ead5ed0860d0120b39b775df3cec5fb19
AccidentallyOnPurpose <4935931+AccidentallyOnPurpose@users.noreply.github.com>
2019-12-21 19:22:45 +0800

    misc

--- generator/gpt2/gpt2_generator.py
+++ generator/gpt2/gpt2_generator.py
@@ -11,27 +11,28 @@ from tqdm import tqdm
 from transformers import GPT2Config
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
+
 from story.utils import *
 
-logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt = '%m/%d/%Y %H:%M:%S',
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
 logger = logging.getLogger(__name__)
 
+debug_print = os.environ.get("AID_DEBUG", False)
+if debug_print:
+    logger.warn("Running in AID_DEBUG=True mode")
+
 warnings.filterwarnings("ignore")
-MAX_LENGTH = int(10000)  # Hardcoded max length to avoid infinite loop
+# MAX_LENGTH = int(10000)  # Hardcoded max length to avoid infinite loop
 MODEL_CLASSES = {
-    'gpt2': (GPT2LMHeadModel, GPT2Tokenizer),
+    "gpt2": (GPT2LMHeadModel, GPT2Tokenizer),
 }
 
-# def set_seed(args):
-#     np.random.seed(args.seed)
-#     torch.manual_seed(args.seed)
-#     if args.n_gpu > 0:
-#         torch.cuda.manual_seed_all(args.seed)
 
-
-def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')):
+def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float("Inf")):
     """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
         Args:
             logits: logits distribution shape (batch size x vocabulary size)
@@ -57,58 +58,107 @@ def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')
         sorted_indices_to_remove[..., 0] = 0
 
         # scatter sorted tensors to original indexing
-        indices_to_remove = sorted_indices_to_remove.scatter(dim=1, index=sorted_indices, src=sorted_indices_to_remove)
+        indices_to_remove = sorted_indices_to_remove.scatter(
+            dim=1, index=sorted_indices, src=sorted_indices_to_remove
+        )
         logits[indices_to_remove] = filter_value
     return logits
 
 
-def sample_sequence(model, length, context, num_samples=1, temperature=1, top_k=0, top_p=0.0, repetition_penalty=1.0,
-                    is_xlnet=False, is_xlm_mlm=False, xlm_mask_token=None, xlm_lang=None, device='cpu'):
+def sample_sequence(
+    model,
+    length,
+    context,
+    num_samples=1,
+    temperature=1,
+    top_k=0,
+    top_p=0.0,
+    repetition_penalty=1.0,
+    is_xlnet=False,
+    is_xlm_mlm=False,
+    xlm_mask_token=None,
+    xlm_lang=None,
+    device="cpu",
+):
     context = torch.tensor(context, dtype=torch.long, device=device)
     context = context.unsqueeze(0).repeat(num_samples, 1)
     generated = context
     with torch.no_grad():
-        for _ in tqdm(range(length), leave=False, desc='generating'):
+        # for _ in tqdm(range(length), leave=False, desc='generating'):
+        for _ in range(length):
 
-            inputs = {'input_ids': generated}
+            inputs = {"input_ids": generated}
             if is_xlnet:
                 # XLNet is a direct (predict same token, not next token) and bi-directional model by default
                 # => need one additional dummy token in the input (will be masked), attention mask and target mapping (see model docstring)
-                input_ids = torch.cat((generated, torch.zeros((1, 1), dtype=torch.long, device=device)), dim=1)
-                perm_mask = torch.zeros((1, input_ids.shape[1], input_ids.shape[1]), dtype=torch.float, device=device)
+                input_ids = torch.cat(
+                    (generated, torch.zeros((1, 1), dtype=torch.long, device=device)),
+                    dim=1,
+                )
+                perm_mask = torch.zeros(
+                    (1, input_ids.shape[1], input_ids.shape[1]),
+                    dtype=torch.float,
+                    device=device,
+                )
                 perm_mask[:, :, -1] = 1.0  # Previous tokens don't see last token
-                target_mapping = torch.zeros((1, 1, input_ids.shape[1]), dtype=torch.float, device=device)
+                target_mapping = torch.zeros(
+                    (1, 1, input_ids.shape[1]), dtype=torch.float, device=device
+                )
                 target_mapping[0, 0, -1] = 1.0  # predict last token
-                inputs = {'input_ids': input_ids, 'perm_mask': perm_mask, 'target_mapping': target_mapping}
+                inputs = {
+                    "input_ids": input_ids,
+                    "perm_mask": perm_mask,
+                    "target_mapping": target_mapping,
+                }
 
             if is_xlm_mlm and xlm_mask_token:
                 # XLM MLM models are direct models (predict same token, not next token)
                 # => need one additional dummy token in the input (will be masked and guessed)
-                input_ids = torch.cat((generated, torch.full((1, 1), xlm_mask_token, dtype=torch.long, device=device)), dim=1)
-                inputs = {'input_ids': input_ids}
+                input_ids = torch.cat(
+                    (
+                        generated,
+                        torch.full(
+                            (1, 1), xlm_mask_token, dtype=torch.long, device=device
+                        ),
+                    ),
+                    dim=1,
+                )
+                inputs = {"input_ids": input_ids}
 
             if xlm_lang is not None:
-                inputs["langs"] = torch.tensor([xlm_lang] * inputs["input_ids"].shape[1], device=device).view(1, -1)
+                inputs["langs"] = torch.tensor(
+                    [xlm_lang] * inputs["input_ids"].shape[1], device=device
+                ).view(1, -1)
 
-            outputs = model(**inputs)  # Note: we could also use 'past' with GPT-2/Transfo-XL/XLNet/CTRL (cached hidden-states)
-            next_token_logits = outputs[0][:, -1, :] / (temperature if temperature > 0 else 1.)
+            outputs = model(
+                **inputs
+            )  # Note: we could also use 'past' with GPT-2/Transfo-XL/XLNet/CTRL (cached hidden-states)
+            next_token_logits = outputs[0][:, -1, :] / (
+                temperature if temperature > 0 else 1.0
+            )
 
             # repetition penalty from CTRL (https://arxiv.org/abs/1909.05858)
             for i in range(num_samples):
                 for _ in set(generated[i].tolist()):
                     next_token_logits[i, _] /= repetition_penalty
 
-            filtered_logits = top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
-            if temperature == 0: # greedy sampling:
+            filtered_logits = top_k_top_p_filtering(
+                next_token_logits, top_k=top_k, top_p=top_p
+            )
+            if temperature == 0:  # greedy sampling:
                 next_token = torch.argmax(filtered_logits, dim=-1).unsqueeze(-1)
             else:
-                next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
+                next_token = torch.multinomial(
+                    F.softmax(filtered_logits, dim=-1), num_samples=1
+                )
             generated = torch.cat((generated, next_token), dim=1)
     return generated
 
 
 class GPT2Generator:
-    def __init__(self, generate_num=60, temperature=0.4, top_k=40, top_p=0.9, censor=True):
+    def __init__(
+        self, generate_num=60, temperature=0.4, top_k=40, top_p=0.9, censor=True
+    ):
         self.generate_num = generate_num
         self.temp = temperature
         self.top_k = top_k
@@ -127,7 +177,7 @@ class GPT2Generator:
         logger.info(f"Using device={self.device}, checkpoint={self.checkpoint_path}")
 
         # Load tokenizer and model
-        model_class, tokenizer_class = MODEL_CLASSES['gpt2']
+        model_class, tokenizer_class = MODEL_CLASSES["gpt2"]
         self.tokenizer = tokenizer_class.from_pretrained(self.checkpoint_path)
         self.model = model_class.from_pretrained(self.checkpoint_path)
         self.model.to(self.device)
@@ -139,10 +189,13 @@ class GPT2Generator:
         except ImportError:
             pass
         else:
-            model = amp.initialize(model, opt_level='O2')
+            model = amp.initialize(model, opt_level="O2")
 
         # context_tokens = self.tokenizer.encode(' ', add_special_tokens=False)
-        context_tokens = [self.tokenizer.pad_token_type_id, self.tokenizer.pad_token_type_id]
+        context_tokens = [
+            self.tokenizer.pad_token_type_id,
+            self.tokenizer.pad_token_type_id,
+        ]
         out = self.sample_sequence(context_tokens).tolist()
         # out = out[:, len(context_tokens):].tolist()
         for o in out:
@@ -169,22 +222,24 @@ class GPT2Generator:
         )
         return out
 
-
     def prompt_replace(self, prompt):
-        # print("\n\nBEFORE PROMPT_REPLACE:")
-        # print(repr(prompt))
+        if debug_print:
+            print("\n\nBEFORE PROMPT_REPLACE:")
+            print(repr(prompt))
         if len(prompt) > 0 and prompt[-1] == " ":
             prompt = prompt[:-1]
 
         # prompt = second_to_first_person(prompt)
 
-        # print("\n\nAFTER PROMPT_REPLACE")
-        # print(repr(prompt))
+        if debug_print:
+            print("\n\nAFTER PROMPT_REPLACE")
+            print(repr(prompt))
         return prompt
 
     def result_replace(self, result):
-        # print("\n\nBEFORE RESULT_REPLACE:")
-        # print(repr(result))
+        if debug_print:
+            print("\n\nBEFORE RESULT_REPLACE:")
+            print(repr(result))
 
         result = cut_trailing_sentence(result)
         if len(result) == 0:
@@ -201,9 +256,9 @@ class GPT2Generator:
         if not first_letter_capitalized:
             result = result[0].lower() + result[1:]
 
-        #
-        # print("\n\nAFTER RESULT_REPLACE:")
-        # print(repr(result))
+        if debug_print:
+            print("\n\nAFTER RESULT_REPLACE:")
+            print(repr(result))
 
         return result
 
@@ -216,7 +271,7 @@ class GPT2Generator:
                 context_tokens
                 # [context_tokens for _ in range(self.batch_size)]
             )
-            out = out[:, len(context_tokens):].tolist()
+            out = out[:, len(context_tokens) :].tolist()
             for o in out:
                 generated += 1
                 text = self.tokenizer.decode(o, clean_up_tokenization_spaces=True)
@@ -225,14 +280,10 @@ class GPT2Generator:
                     if index == -1:
                         index = None
                     text = text[:index]
-            # for i in range(self.batch_size):
-            #     generated += 1
-            #     text = self.enc.decode(out[i])
         return text
 
     def generate(self, prompt, options=None, seed=1):
 
-        debug_print = False
         prompt = self.prompt_replace(prompt)
 
         if debug_print:
